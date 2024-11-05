bdbdf393ca6f351747a54e721e081a5c22a4a1f5
Abdulmalik Elghedafi <2420753@uwtsd@ac.uk>
2024-11-05 12:50:25 +0000

    fixed bugs, code works git status

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,56 @@ public class Main {
     public static final Scanner stdin = new Scanner(System.in);
     public static void main(String[] args) {
 
-        System.out.printf("Enter first number: ");
-        int firstNumber = stdin.nextInt();
+        while (true) {
+            int option = menu();
 
-        System.out.printf("Enter second number: ");
-        int secondNumber = stdin.nextInt();
+            if (option == 5) {
+                System.out.print("Program will now terminate! ");
+                break;
+            }
+
+            System.out.print("Enter first number: ");
+            int firstNumber = stdin.nextInt();
+
+            System.out.print("Enter second number: ");
+            int secondNumber = stdin.nextInt();
 
 
+            switch (option) {
+                case 1:
+                    int adding = add(firstNumber, secondNumber);
+                    System.out.printf("Result is: " + adding);
+                    break;
+                case 2:
+                    int subtracting = subtract(secondNumber, firstNumber);
+                    System.out.printf("Result is: " + subtracting);
+                    break;
+                case 3:
+                    System.out.printf("Result is: " + multiply(firstNumber, secondNumber));
+                    break;
+                case 4:
+                    System.out.printf("Result is: " + divide(firstNumber, secondNumber));
+                    break;
+                case 5:
+                    System.out.print("Program will now terminate!");
+                    break;
 
-        switch (displayMenu()) {
-            case 1:
-                System.out.printf("Result is: " + add(firstNumber, secondNumber));
-                break;
-            case 2:
-                System.out.printf("Result is: " + subtract(firstNumber, secondNumber));
-                break;
-            case 3:
-                System.out.printf("Result is: " + multiply(firstNumber, secondNumber));
-                break;
-            case 4:
-                System.out.printf("Result is: " + divide(firstNumber, secondNumber));
-                break;
-            case 5:
-                System.out.print("Program will now terminate!");
-                break;
+            }
+            break;
 
         }
 
-        int option = menu();
+
+
+
+        // Code works, although not the output I'm wanting.
+        // I want the menu to run first, then once the user has selected an option.
+        // The program will calculate the value and print it off the user
+        // If you run the program, it will show you the mess I'm on about.
+
+
+
+
 
 
         //int result = add(3, 5);
@@ -84,7 +106,7 @@ public class Main {
 
             try {
                 numberEntered = Integer.parseInt(stdin.nextLine());
-                System.out.println("You're input is valid!");
+                //System.out.println("You're input is valid!");
                 readNumber = false;
             } catch (NumberFormatException e) {
                 System.out.print("Please enter a number, try again:\n ");
@@ -104,6 +126,8 @@ public class Main {
         System.out.println("3. Multiply");
         System.out.println("4. Divide");
         System.out.println("5. Exit");
+
+
         return selectOption();
     }
 
